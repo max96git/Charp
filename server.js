e82b32e3be073f810d8b2f4116200e86c52c727c
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-05 13:55:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Serve static files from the src folder
-app.use(express.static(path.join(__dirname, 'src')));
-
 app.get('/', (req, res) => {
-    console.log('Home page requested');
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+    res.send('Hello World');
 });
 
-app.get('/avatar', (req, res) => {
-    console.log('Avatar page requested');
-    res.sendFile(path.join(__dirname, 'src', 'avatar.html'));
-});
-
-app.get('/game-play', (req, res) => {
-    console.log('Game play page requested');
-    res.sendFile(path.join(__dirname, 'src', 'pages', 'game-play.html'));
-});
-
-app.get('/create-game', (req, res) => {
-    console.log('Create game page requested');
-    res.sendFile(path.join(__dirname, 'src', 'pages', 'create-game.html'));
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });

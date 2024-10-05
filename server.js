cf351a5f168de64777520e08fab5511fa36a039a
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-05 14:44:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
+app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Route to fetch games
+// Route to fetch all games
 app.get('/api/games', fetchGames);
 
+// Route to create a new game
+app.post('/api/games', createGame);
+
+// Request Pages
 app.get('/', (req, res) => {
     console.log('Home page requested');
     res.sendFile(path.join(__dirname, 'src', 'index.html')).catch(err => {
